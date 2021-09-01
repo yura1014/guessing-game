8139665b892e4a0fb7e38e10a6811db18bc510d5
Yura Alexeev <yurals1014@gmail.com>
2021-09-01 18:18:06 +0300

    init: guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,80 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.arr = [];
+    this.begin = 0;
+    this.end = 0;
+    this.middle = 0;
+    this.firstTime = true;
+    this.goLower;
+    this.goGreater;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.begin = min;
+    this.end = max;
+  }
 
-    }
+  guess() {
+    return Math.ceil((this.begin + this.end) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.end = this.guess();
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.begin = this.guess();
+  }
 }
 
+const number = 648;
+const game = new GuessingGame();
+game.setRange(0, 4010);
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+console.log(result);
 module.exports = GuessingGame;
+
+/*
+0 4048
+2024
+game.lower();
+0 1011
+game.lower();
+0 505
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower(); //will be 380 505
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+*/
